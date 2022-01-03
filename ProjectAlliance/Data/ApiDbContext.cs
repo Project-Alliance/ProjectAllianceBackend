bf18b68fe63c2a6d7ed0c78613517f9570b19b3c
as1265513 <60047565+as1265513@users.noreply.github.com>
2022-01-03 20:52:42 +0500

    BAckend
    backend

---
+++ ProjectAlliance/Data/ApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProjectAlliance.Models;
+
+namespace ProjectAlliance.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext>options):base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(e => e.Property(e => e.age).HasColumnType("tinyint(1)").HasConversion<short>());
+
+        }
+    }
+}
---
+++ ProjectAlliance/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ProjectAlliance.Models
+{
+    public class User
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string password { set; get; }
+        public string userName { set; get; }
+        public string email { set; get; }
+        public int age { set; get; }
+        public User()
+        {
+        }
+    }
+}
